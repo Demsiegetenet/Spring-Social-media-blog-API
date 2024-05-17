9d5cc53ebab32a54329517854650eedde6c48086
Getenet Demsie <demiseget@gmail.com>
2024-05-17 17:06:45 +0000

    "first commit"

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,16 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@Controller
 public class SocialMediaController {
 
+    @Autowired
+   AccountService accountService;
+
+   @PostMapping("/register")
+   public ResponseEntity<String> registerAccount(@RequestBody Account account){
+    accountService.registerAccount(account);
+    return ResponseEntity.status(200).body("suceefully regstered");
+
+   }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,32 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import java.util.*;
+@Service
 public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Account registerAccount(Account account){
+    List<Account> accounts = accountRepository.findAll();
+   
+    if(account.getUsername().length()>0 && account.getPassword().length()>=4){
+        for(Account acc:accounts){
+            if(acc.getAccountId()!=account.getAccountId()){
+                accountRepository.save(account);
+                return account;
+            }
+            else
+            return null;
+        }
+    }
+   
+     return null;
+    }
 }
