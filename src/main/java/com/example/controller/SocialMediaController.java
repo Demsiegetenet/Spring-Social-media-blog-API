99d46dcb3581d695f95746f0fabc4e5a2df94a51
Getenet Demsie <demiseget@gmail.com>
2024-05-24 17:16:25 +0000

    "update method updated"

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,9 +1,6 @@
 package com.example.controller;
 
 import java.util.List;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.example.entity.Account;
 import com.example.entity.Message;
@@ -41,38 +37,37 @@ public class SocialMediaController {
 
    @PostMapping("/register")
    public ResponseEntity<Account> registerAccount(@RequestBody Account account){
-    List<Account> accounts = accountService.getAllAccounts();
- 
-    for(Account acc:accounts){
-      if(account.getUsername().equals(acc.getUsername()))
-      return ResponseEntity.status(409).build();
-      else{
-         if(account.getUsername().length()>0 && account.getPassword().length()>=4){
-            Account registeredAccount = accountService.registerAccount(account);
-           return ResponseEntity.status(200).body(registeredAccount);
+         List<Account> accounts = accountService.getAllAccounts();
+      
+         for(Account acc:accounts){
+               if(account.getUsername().equals(acc.getUsername()))
+                  return ResponseEntity.status(409).build();
+               else{
+                  if(account.getUsername().length()>0 && account.getPassword().length()>=4){
+                     Account registeredAccount = accountService.registerAccount(account);
+                     return ResponseEntity.status(200).body(registeredAccount);
+                  }
+               }
          }
-      }
-    }
-    return ResponseEntity.status(400).build();
-
+   return ResponseEntity.status(400).build();
    }
 
    @PostMapping("/login")
    public ResponseEntity<Account> loginAccount(@RequestBody Account account){
       Account loginAccount = accountService.loginAccount(account);
       if(loginAccount!=null)
-      return ResponseEntity.status(200).body(loginAccount);
+         return ResponseEntity.status(200).body(loginAccount);
       else
-      return ResponseEntity.status(401).build();
+         return ResponseEntity.status(401).build();
    }
 
    @PostMapping("/messages")
    public ResponseEntity<Message> createMessgae(@RequestBody Message message){
        Message addedMessage = messageService.addMessage(message);
        if(addedMessage!=null)
-       return ResponseEntity.status(200).body(addedMessage);
+          return ResponseEntity.status(200).body(addedMessage);
        else
-       return ResponseEntity.status(409).body(addedMessage);
+          return ResponseEntity.status(409).body(addedMessage);
 
    }
 
@@ -82,11 +77,11 @@ public class SocialMediaController {
    }
 
    @PatchMapping("/messages/{message_id}")
-   public ResponseEntity<Message> updateMessage(@PathVariable int message_id, Message message){
-      Message updatedMessage = messageService.updateMesage(message_id,message);
+   public ResponseEntity<Integer> updateMessage(@RequestBody Message message, @PathVariable int message_id){
+      Message updatedMessage = messageService.updateMesage(message,message_id);
       if(updatedMessage!=null)
-      return ResponseEntity.status(200).body(updatedMessage);
+          return ResponseEntity.status(200).body(1);
       else
-      return ResponseEntity.status(400).build();
+          return ResponseEntity.status(400).build();
    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.entity.Account;
 import com.example.entity.Message;
-import com.example.repository.AccountRepository;
+import com.example.repository.AccountRepository; 
 import com.example.repository.MessageRepository;
 
 @Service
@@ -20,8 +20,6 @@ public class MessageService {
     @Autowired
     AccountRepository accountRepository;
 
- 
-
     public MessageService(MessageRepository messageRepository){
         this.messageRepository = messageRepository;
     }
@@ -29,22 +27,6 @@ public class MessageService {
     public List<Message> getAllMessages(){
         return messageRepository.findAll();
     }
-
-    // public Message addMessage(Message newMessage){
-    //     List<Message> messages = messageRepository.findAll();
-    //     for(Message msg:messages){
-    //      if(newMessage.getPostedBy()!=msg.getPostedBy()){
-    //          if(newMessage.getMessageText().length()>0&&newMessage.getMessageText().length()<255){
-    //              Message message = new Message();
-    //                      message.setMessageText(newMessage.getMessageText());
-    //                      message.setPostedBy(newMessage.getPostedBy());
-    //                      message.setTimePostedEpoch(newMessage.getTimePostedEpoch());
-    //              return messageRepository.save(message);
-    //          }
-    //      }
-    //     }
-    //     return null;
-    //  }
 
      public Message addMessage(Message message) {
         List<Account> accounts = accountRepository.findAll();
@@ -63,27 +45,18 @@ public class MessageService {
          }
     return null;
 }
-
-     public void deleteMessageById(){
-
-     }
-
      public Optional<Message> getMessageByPostedBy(int posted_by){
         return messageRepository.findById(posted_by);
         }
 
-        public Message updateMesage(int message_id, Message message){
+        public Message updateMesage(Message message,int message_id){
             List<Message> messages = messageRepository.findAll();
             for(int i=0;i<messages.size();i++){
                 if(message_id==messages.get(i).getMessageId()){
-                if(message.getMessageText().length()>0 && message.getMessageText().length()<=255){
-                    Message m = new Message();
-                        
-                         m.setMessageText(message.getMessageText());
-                         m.setPostedBy(message.getPostedBy());
-                         m.setTimePostedEpoch(message.getTimePostedEpoch());
-                         return messageRepository.save(m);
-
+                   if(message.getMessageText().length()>0 && message.getMessageText().length()<=255){
+                        messages.get(i).setMessageText(message.getMessageText());                   
+                        messageRepository.save(messages.get(i));
+                        return messages.get(i);
                     }
                 }
                 }
